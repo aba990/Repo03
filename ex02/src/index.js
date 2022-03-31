9ae3b2369fad7e0dcb74472d2dd10d02b240961d
AnelB <anel.b@live.com>
2022-03-31 19:31:31 +0200

    drugo ubacivanje

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,11 +1,11 @@
 function main() {
-    // Only change code below this line
-    //sarajevo;
-    /* dark chocolate */
-   // Only change code above this line
-    
- }
+  // Only change code below this line
+  // sarajevo ljubavi moja
+  /*ko nekad u osam*/
+  // Only change code above this line
 
- //console.log(main());
+}
 
- module.exports = main;
+console.log(main());
+
+module.exports = main;
